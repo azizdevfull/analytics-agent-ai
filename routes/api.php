141f01975f511a48bac9937fbcdfe5489dd8838a
Azizbek Isroilov <aziz16110904@gmail.com>
2026-03-09 17:44:56 +0500

    Removed Laravel AI package and related code

--- routes/api.php
+++ routes/api.php
@@ -1,6 +1,5 @@
 <?php
 
-use App\Http\Controllers\AnalyticController;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
 
@@ -8,6 +7,3 @@ Route::get('/user', function (Request $request) {
     return $request->user();
 })->middleware('auth:sanctum');
 
-Route::post('/analytic/orders', [AnalyticController::class, 'orderAnalytic']);
-Route::post('/analytic/orders/continue', [AnalyticController::class, 'orderAnalyticContinue']);
-Route::post('/analytic/orders/stream', [AnalyticController::class, 'orderAnalyticStream']);
