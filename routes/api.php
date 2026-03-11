6bb83626c4b8e2c3fbdd68662f118e1178e4d8cd
Azizbek Isroilov <aziz16110904@gmail.com>
2026-03-11 05:58:48 +0500

    `Removed OrderAiController and its route`

--- routes/api.php
+++ routes/api.php
@@ -1,11 +1,8 @@
 <?php
 
-use App\Http\Controllers\OrderAiController;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
 
 Route::get('/user', function (Request $request) {
     return $request->user();
 })->middleware('auth:sanctum');
-
-Route::post('/orders/chat/start', [OrderAiController::class, 'store']);
